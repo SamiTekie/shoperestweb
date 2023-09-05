69629bd50297b95196b52b4e5e12309923059810
SamiTekie <samitekie84@gmail.com>
2023-09-05 16:08:17 +0300

    DataBase update productCategory table.

--- src/DataBaseShope/shope.sql
+++ src/DataBaseShope/shope.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 04, 2023 at 04:25 PM
+-- Generation Time: Sep 05, 2023 at 03:05 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -42,12 +42,9 @@ CREATE TABLE `product` (
 INSERT INTO `product` (`product_id`, `product_name`, `product_description`, `product_price`, `product_category_id`) VALUES
 (5, 'Adidas', 'Addias new fashion', 44.5, 2),
 (6, 'Adidas', 'Addias new fashion', 55.8, 2),
-(9, 'UnderArmer ', 'NUnder armer  2023', 55.8, 1),
-(10, 'UnderArmer ', 'NUnder armer  2023', 55.8, 1),
 (12, 'Updated Product Versache/Gucci ', 'Updated Product Description', 19.99, 2),
 (15, 'Updated Product', 'This is an updated product', 19.99, 2),
-(16, 'New Product', 'This is a new product', 9.99, 1),
-(18, 'Updated Product', 'Updated product description', 19.99, 2);
+(19, 'Adidas', 'Addias new fashion', 44.5, 2);
 
 -- --------------------------------------------------------
 
@@ -57,18 +54,18 @@ INSERT INTO `product` (`product_id`, `product_name`, `product_description`, `pro
 
 CREATE TABLE `product_category` (
   `product_category_id` bigint(20) NOT NULL,
-  `product_category_name` varchar(255) NOT NULL
+  `product_category_name` varchar(255) NOT NULL,
+  `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `product_category`
 --
 
-INSERT INTO `product_category` (`product_category_id`, `product_category_name`) VALUES
-(1, 'Updated Category'),
-(2, 'Sport Shoes'),
-(3, 'sport Wear'),
-(5, 'Example Category');
+INSERT INTO `product_category` (`product_category_id`, `product_category_name`, `active`) VALUES
+(2, 'Sport Shoes', 1),
+(3, 'sport Wear', 1),
+(5, 'Example Category', 1);
 
 -- --------------------------------------------------------
 
@@ -156,13 +153,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `product_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `product_category`
 --
 ALTER TABLE `product_category`
-  MODIFY `product_category_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `product_category_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `role`
